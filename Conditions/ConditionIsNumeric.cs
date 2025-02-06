d954503282988b2c087b91f72c6450ad8d087590
Dan Munteanu <epsdel@gmail.com>
2025-02-06 13:31:48 +0200

    ConditionIsNumeric

---
+++ Conditions/ConditionIsNumeric.cs
@@ -0,0 +1,21 @@
+﻿namespace Memphis.Conditions
+{
+    public class ConditionIsNumeric : TokenCondition
+    {
+        public override bool Evaluate(MPSToken token)
+        {
+            if (token == null)
+                return false;
+
+            if (token.Text == null)
+                return false;
+
+            return int.TryParse(token.Text, out _);
+        }
+
+        public override string Description()
+        {
+            return "IsNumeric";
+        }
+    }
+}
