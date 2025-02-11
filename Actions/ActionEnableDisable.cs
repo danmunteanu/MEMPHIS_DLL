5301e61f3e7eafc8a9a5701e8c890f0ccc7962e2
Dan Munteanu (ASUS) <epsdel@gmail.com>
2025-02-11 18:49:38 +0200

    Action Enable/Disable

---
+++ Actions/ActionEnableDisable.cs
@@ -0,0 +1,27 @@
+﻿namespace Memphis.Actions
+{
+    public class ActionEnableDisable : TokenAction
+    {
+        public bool Disable { get; set; } = false;  //  Enable by default
+
+        EngineBase? Engine { get; set; } = null;
+
+        public ActionEnableDisable(EngineBase engineBase, bool disable = false)
+        {
+            Disable = disable;
+            Engine = engineBase;
+        }
+
+        public override void Execute(Token token)
+        {
+            if (token == null)
+                return;
+
+            if (Engine == null)
+                return;
+
+            if (!Engine.IsTokenCurrentRoot(token))
+                token.Discard = Disable;
+        }
+    }
+}
