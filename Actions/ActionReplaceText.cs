3b60a295510c4157f8c38be932d336636064c40f
Dan Munteanu (ASUS) <epsdel@gmail.com>
2025-02-11 19:14:42 +0200

    ActionReplaceText, SetSeparators, Condition Is Root

---
+++ Actions/ActionReplaceText.cs
@@ -0,0 +1,29 @@
+﻿namespace Memphis.Actions
+{
+    public class ActionReplaceText : TokenAction
+    {
+        public string Search {  get; set; } = string.Empty;
+
+        public string Replace {  get; set; } = string.Empty;
+
+        private EngineBase? mEngine = null;
+
+        public ActionReplaceText(EngineBase engine, string search = "", string replace = "")
+        {
+            mEngine = engine;
+            Search = search;
+            Replace = replace;
+        }
+        public override void Execute(Token token)
+        {
+            if (token == null)
+                return;
+
+            if (string.IsNullOrEmpty(token.Text))
+                return;
+
+            string result = token.Text.Replace(Search, Replace);
+            token.Text = result;
+        }
+    }
+}
---
+++ Actions/ActionSetSeparators.cs
@@ -0,0 +1,28 @@
+﻿namespace Memphis.Actions
+{
+    public class ActionSetSeparators : TokenAction
+    {
+        public string Separators { get; set; } = string.Empty;
+
+        private EngineBase? mEngine = null;
+
+        public ActionSetSeparators(EngineBase engine, string separators = "") 
+        { 
+            mEngine = engine;
+            Separators = separators;
+        }
+
+        public override void Execute(Token token)
+        {
+            if (mEngine == null)
+                throw new Exception("Engine is null");
+
+            if (token == null)
+                throw new ArgumentNullException("Token is null");
+
+            token.Separators = Separators;
+
+            mEngine.Update(token);
+        }
+    }
+}
