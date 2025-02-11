573bef9186eb8e190cb79a95cde255312d0ba5c4
Dan Munteanu (ASUS) <epsdel@gmail.com>
2025-02-11 18:58:21 +0200

    Added ActionInsertText

---
+++ Actions/ActionInsertText.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Memphis.Actions
+{
+    public class ActionInsertText : TokenAction
+    {
+        public ActionInsertText(string textToAdd = "", int position = int.MaxValue) 
+        { 
+        }
+
+        public string TextToAdd {  get; set; } = string.Empty;
+        public int Position { get; set; } = int.MaxValue;
+
+        public override void Execute(Token token)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            sb.Append("InsertText (\"");
+            sb.Append(TextToAdd);
+            sb.Append("\") on Position ");
+            sb.Append(Position);
+            return sb.ToString();
+        }
+    }
+}
